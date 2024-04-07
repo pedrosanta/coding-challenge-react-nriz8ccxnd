7a27ee3603f198c574e7940ccfcff29d47a32c77
Pedro Machado Santa <pedro.santa@gmail.com>
2024-04-07 19:30:45 +0100

    Add material ui
    Using CssBaseline component as CSS reset.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import CssBaseline from '@mui/material/CssBaseline';
+
 import './App.css';
 
 function App() {
   return (
-    "APP"
+    <CssBaseline>
+      APP
+    </CssBaseline>
   );
 }
 
