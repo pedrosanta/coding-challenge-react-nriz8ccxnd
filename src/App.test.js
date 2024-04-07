93772a774d3616280a46ab473e8040b136b2cb98
Pedro Machado Santa <pedro.santa@gmail.com>
2024-04-07 19:20:23 +0100

    Remove main app component CRA placeholder content

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders placeholder element', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const placeholderElement = screen.getByText(/APP/i);
+  expect(placeholderElement).toBeInTheDocument();
 });
