bfd75ddea086ad15d6875c1de0000b113403ab9d
Pedro Machado Santa <pedro.santa@gmail.com>
2024-04-07 21:47:14 +0100

    Add muit app/nav bar

--- src/routes/RootRoute.js
+++ src/routes/RootRoute.js
@@ -1,17 +1,50 @@
 import {
-  Link,
+  NavLink,
   Outlet
 } from "react-router-dom";
+
+import {
+  AppBar,
+  Toolbar,
+  Typography
+} from "@mui/material";
+
+import { styled } from "@mui/material/styles";
+
+const NavItem = styled(Typography)(({theme}) => ({
+  color: "#fff",
+  textDecoration: 'none',
+  marginRight: theme.spacing(3),
+  'a.active > &': {
+    textDecoration: 'underline'
+  }
+}));
 
 function RootRoute() {
   return (
     <>
-      <nav>
-        <ul>
-          <li><Link to={''}>Tasks</Link></li>
-          <li><Link to={'users'}>Users</Link></li>
-        </ul>
-      </nav>
+      <AppBar>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              marginRight: 4
+            }}
+          >
+            React Coding Challenge
+          </Typography>
+
+          <NavLink to={""} style={{ textDecoration: 'none'}}>
+            <NavItem>Tasks</NavItem>
+          </NavLink>
+
+          <NavLink to={"users"} style={{ textDecoration: 'none'}}>
+            <NavItem>Users</NavItem>
+          </NavLink>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
       <Outlet />
     </>
   );
